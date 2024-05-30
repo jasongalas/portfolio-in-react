e47d3bed8b5562dacea556bf491c42efafb8dd55
jasongalas <jasongalas@gmail.com>
2024-05-29 22:01:06 -0700

    live link added, weird name, and fixed my ME image

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,9 @@ import Me from '../assets/images/Me.jpeg';
 
 export default function About() {
   return (
-    <div>
+    <div className='aboutme-container'>
       <h2>About The Coder</h2>
-      <img src={Me} alt="Me" className="aboutme-container" />
+      <img src={Me} alt="Me" className="aboutme-image" />
       <p className='blurb-container'>
         The spring of 2024 was a time spent trying to hone the act of coding. 
         In 3 short months, our cohort was crammed with so many topics and ideas,
